2bf43e4888bcf21cc2a5467d65b414c23a8903ed
Tomas Miranda <155570266+tomasvbm@users.noreply.github.com>
2024-03-01 10:37:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ if (window.location.href.includes('https://taxifare.lewagon.com')) {
   taxiFareApiUrl = 'https://taxifare.lewagon.ai/predict';
 }
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoia3Jva3JvYiIsImEiOiJja2YzcmcyNDkwNXVpMnRtZGwxb2MzNWtvIn0.69leM_6Roh26Ju7Lqb2pwQ';
+mapboxgl.accessToken = 'pk.eyJ1IjoidG9tYXN2Ym0iLCJhIjoiY2x0OGduOW1qMHBvbjJqbXNoNm55c3JnZyJ9.5ft6vuyP6Yehpn3oUtNLlw';
 
 const displayMap = (start, stop) => {
   const map = new mapboxgl.Map({
